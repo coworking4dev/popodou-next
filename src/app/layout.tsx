ee3fd94994b72e269f276b151a0af97576421e04
pqr4579 <dlwjdwls6504@gmail.com>
2025-09-16 11:27:41 +0900

    fix : only use light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,7 +105,7 @@ export default function RootLayout({
       <body>
         <ErrorBoundary>
           <AppProvider>
-            <ThemeProvider>
+            <ThemeProvider forcedTheme="light">
               <OverlayProvider>
                 {children}
                 <Toaster />
